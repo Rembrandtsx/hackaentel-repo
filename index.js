0b39e104de0326c925654d8b0df5dac1ec24b202
Santiago Salazar <s.salazara@uniandes.edu.co>
2020-07-18 20:20:29 -0500

    Skeleton of the functions to use.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-// IMPORTS
+// IMPORTS ===============================================
 const bodyParser = require('body-parser');
+const fetch = require('node-fetch');
 
 const express = require('express');
 const app = express.app();
@@ -7,16 +8,39 @@ const app = express.app();
 // CONSTANTS - GLOBAL VARS
 const port = process.env.PORT || 8080;
 
-// CONFIG
+// CONFIG ===============================================
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// ROOT POINTS
+// ROOT POINTS ===============================================
 app.get('/', (req, res, next) => {
-    res.status(200).send('Hello world!');
+    res.status(200).send('GO MAVERICK!');
 });
+// Target
+app.get('/api/audience', sendAudience);
+// sms
+app.get('/api/sendSMS', sendSMS);
+// link
+app.get('/api/link', sendLink);
 
-// LISTEN SERVER
+// LISTEN SERVER ===============================================
 app.listen(port, () => {
     console.log('App listening on port ' + port);
 });
+
+
+// =============================================================
+// =============================================================
+// Funciones
+
+async function sendAudience(req, res, next) {
+    
+}
+
+async function sendSMS(req, res, next) {
+
+}
+
+async function sendLink(req, res, next) {
+    
+}
