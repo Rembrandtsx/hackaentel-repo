8e0aea2d35f9db33602d4c962b6f10f05140737d
Camilo Salinas <iam@camilosalinas.me>
2020-07-19 15:30:45 -0500

    Build

---
+++ front/build/precache-manifest.b1169de8b28106779024d1499af522a7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2bde41ae85bcbc847ed9334920acd7b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "91bb0cb8cfbe03596221",
+    "url": "/static/css/main.14f7cf1e.chunk.css"
+  },
+  {
+    "revision": "451350387bbf3c8dc7ad",
+    "url": "/static/js/2.0a5f477a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.0a5f477a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91bb0cb8cfbe03596221",
+    "url": "/static/js/main.c8b7f86a.chunk.js"
+  },
+  {
+    "revision": "23f7e4d2c8397ce55bf6",
+    "url": "/static/js/runtime-main.5fe610d3.js"
+  },
+  {
+    "revision": "ff3dbbe20c735d99d94232aae9d71670",
+    "url": "/static/media/entel.ff3dbbe2.svg"
+  },
+  {
+    "revision": "09c8ea60a444b43c3bc3cc79d1b87429",
+    "url": "/static/media/medicine.09c8ea60.svg"
+  }
+]);
