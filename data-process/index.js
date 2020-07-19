8f2a12d6a93885e3a345d61a6672f347db3812c6
Santiago Salazar <s.salazara@uniandes.edu.co>
2020-07-18 23:31:03 -0500

    Changes in file formatting and implemented 2 of 5 endpoints.

--- data-process/index.js
+++ data-process/index.js
@@ -1,25 +1,33 @@
-const csvFilePath='positivos_covid.csv'
-const csv=require('csvtojson')
-const fs = require("fs")
-let deptos={}
-let provinc={}
-let district={}
+const csvFilePath = 'positivos_covid.csv';
+const csv = require('csvtojson');
+const fs = require("fs");
+
+let deptos = {};
+let provinc = {};
+let district = {};
+
 csv()
-.fromFile(csvFilePath)
+.fromFile(csvFilePath, { encoding: "latin1" })
 .then((jsonObj)=>{
     jsonObj.forEach(el=>{
         deptos[el.DEPARTAMENTO] = (deptos[el.DEPARTAMENTO] || 0)+1;
         provinc[el.PROVINCIA] = (provinc[el.PROVINCIA] || 0)+1;
         district[el.DISTRITO] = (district[el.DISTRITO] || 0)+1;
+    });
 
-    })
+    let deptosArr = [];
+    let provincArr = [];
+    let districtArr = [];
+    for (var dep in deptos) {
+        deptosArr.push({name: dep[0] + dep.slice(1).toLowerCase(), value: deptos[dep]});
+    }
+    for (var pro in provinc) {
+        provincArr.push({name: pro[0] + pro.slice(1).toLowerCase(), value: provinc[pro]});
+    }
+    for (var dis in district) {
+        districtArr.push({name: dis[0] + dis.slice(1).toLowerCase().capita, value: district[dis]});
+    }
 
-    fs.writeFileSync("result.json",JSON.stringify( {departamentos: deptos, provincias: provinc, distritos: district}))
+    fs.writeFileSync("../result.json",JSON.stringify( {Departamento: deptosArr, Provincia: provincArr, Distrito: districtArr}))
 
-    /**
-     * [
-     * 	{a:"1", b:"2", c:"3"},
-     * 	{a:"4", b:"5". c:"6"}
-     * ]
-     */ 
-})
+});
--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 // IMPORTS ===============================================
 const bodyParser = require('body-parser');
 const fetch = require('node-fetch');
+const fs = require('fs');
 
 const express = require('express');
-const app = express.app();
+const app = express();
 
 // CONSTANTS - GLOBAL VARS
 const port = process.env.PORT || 8080;
+const API = 'https://zur1he9mqc.execute-api.us-east-1.amazonaws.com/v7';
 
 // CONFIG ===============================================
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -18,10 +20,14 @@ app.get('/', (req, res, next) => {
 });
 // Target
 app.get('/api/audience', sendAudience);
+// Clients
+app.get('/api/clients', sendClients);
 // sms
 app.get('/api/sendSMS', sendSMS);
 // link
 app.get('/api/link', sendLink);
+// getJson
+app.get('/api/json', sendJSON);
 
 // LISTEN SERVER ===============================================
 app.listen(port, () => {
@@ -34,6 +40,46 @@ app.listen(port, () => {
 // Funciones
 
 async function sendAudience(req, res, next) {
+    let { threshold, localization } = req.query;
+
+    try {
+        if(threshold && localization) {
+            let search = fs.readFileSync('result.json');
+            search = JSON.parse(search);
+
+            if(threshold.startsWith("top") && threshold.length > 3 && Number.isInteger(threshold.slice(4))) {
+                let specific = search[localization];
+                if(specific) {
+                    specific.sort((a, b) => b.value - a.value);
+                    res.status(200).send(specific.slice(0, threshold.slice(4)));
+                }
+                else
+                    error(res, 400, "Incorrect query param 'localization'");
+            }
+            else if (threshold.startsWith("ultimos") && threshold.length > 7 && Number.isInteger(threshold.slice(8))) {
+                let specific = search[localization];
+                if(specific) {
+                    specific.sort((a, b) => a.value - b.value);
+                    res.status(200).send(specific.slice(0, threshold.slice(8)));
+                }
+                else
+                    error(res, 400, "Incorrect query param 'localization'");
+            }
+            else {
+                error(res, 400, "Incorrect query param 'threshold'");
+            }
+        }   
+        else {
+            error(res, 400, "Missing mandatory query params 'threshold' or 'localization'");
+        }
+
+    } catch (err) {
+        error(res, 500, err);
+        console.error(err);
+    }
+}
+
+async function sendClients(req, res, next) {
     
 }
 
@@ -42,5 +88,23 @@ async function sendSMS(req, res, next) {
 }
 
 async function sendLink(req, res, next) {
-    
+
+}
+
+async function sendJSON(req, res, next) {
+    try {
+        let search = fs.readFileSync('result.json');
+        search = JSON.parse(search);
+        res.status(200).send(search);
+    } catch (err) {
+        error(res, 500, err);
+        console.error(err);
+    }
+}
+
+function error(res, code, error) {
+    res.status(code).send({ 
+        code: code,
+        error: error
+    });
 }
